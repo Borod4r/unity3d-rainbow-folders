72afb1a1575dfd37ca0753dbc9cc31ca569c833d
Taras Leskiv <leskiv.taras@gmail.com>
2016-01-28 00:26:08 +0100

    added all colors

--- Assets/RainbowFoldersAsset/RainbowFolders/Editor/Menu/RainbowFoldersProjectContextMenu.cs
+++ Assets/RainbowFoldersAsset/RainbowFolders/Editor/Menu/RainbowFoldersProjectContextMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 using Borodar.RainbowFolders.Editor.Settings;
 using UnityEditor;
 
@@ -11,15 +12,34 @@ namespace Borodar.RainbowFolders.Editor
     {
         private const string COLORIZE_MENU = "Assets/Rainbow Folders/Colorize/";
 
-        private const string COLORIZE_RED = COLORIZE_MENU + "Red";
-        private const string COLORIZE_VERMILION = COLORIZE_MENU + "Vermilion";
+        private const string RED = COLORIZE_MENU + "Red";
+        private const string VERMILION = COLORIZE_MENU + "Vermilion";
+        private const string ORANGE = COLORIZE_MENU + "Orange";
+        private const string YELLOW_ORANGE = COLORIZE_MENU + "Yellow-Orange";
+        private const string YELLOW = COLORIZE_MENU + "Yellow";
+        private const string LIME = COLORIZE_MENU + "Lime";
+        private const string GREEN = COLORIZE_MENU + "Green";
+        private const string BONDI_BLUE = COLORIZE_MENU + "Bondi Blue";
+        private const string BLUE = COLORIZE_MENU + "Blue";
+        private const string INDIGO = COLORIZE_MENU + "Indigo";
+        private const string VIOLET = COLORIZE_MENU + "Violet";
+        private const string MAGENTA = COLORIZE_MENU + "Magenta";
 
-        [MenuItem(COLORIZE_RED)]
-        public static void ContextMenuColorizeRed() { ColorizeSelectedFolder();}
-        [MenuItem(COLORIZE_VERMILION)]
-        public static void ContextMenuColorizeVermilion() { ColorizeSelectedFolder(); }
 
-        public static void ColorizeSelectedFolder()
+        [MenuItem(RED)] static void Red() { Colorize(FolderColors.Red);}
+        [MenuItem(VERMILION)] static void Vermilion() { Colorize(FolderColors.Vermilion); }
+        [MenuItem(ORANGE)] static void Orange() { Colorize(FolderColors.Orange); }
+        [MenuItem(YELLOW_ORANGE)] static void YellowOrange() { Colorize(FolderColors.YellowOrange); }
+        [MenuItem(YELLOW)] static void Yellow() { Colorize(FolderColors.Yellow); }
+        [MenuItem(LIME)] static void Lime() { Colorize(FolderColors.Lime); }
+        [MenuItem(GREEN)] static void Green() { Colorize(FolderColors.Green); }
+        [MenuItem(BONDI_BLUE)] static void BondiBlue() { Colorize(FolderColors.BondiBlue); }
+        [MenuItem(BLUE)] static void Blue() { Colorize(FolderColors.Blue); }
+        [MenuItem(INDIGO)] static void Indigo() { Colorize(FolderColors.Indigo); }
+        [MenuItem(VIOLET)] static void Violet() { Colorize(FolderColors.Violet); }
+        [MenuItem(MAGENTA)] static void Magenta() { Colorize(FolderColors.Magenta); }
+
+        public static void Colorize(FolderColors color)
         {
             FolderColorsContainer.Load();
             var selectedObj = Selection.activeObject;
@@ -42,13 +62,21 @@ namespace Borodar.RainbowFolders.Editor
             }
 
             var path = AssetDatabase.GetAssetPath(asset);
-            Debug.Log(asset.name + "   " + path);
-
-            if (AssetDatabase.IsValidFolder(path))
+            if (!AssetDatabase.IsValidFolder(path))
             {
-                Debug.Log("Colorizing " + path);
-                var iconsForFolder = FolderColorsContainer.Load().GetFolderByColor(FolderColors.Red);
-                RainbowFoldersSettings.Load().Folders.Add(new RainbowFolder
+                Debug.LogWarning("Can only colorize folders");
+                return;
+            }
+
+            Debug.Log("Colorizing " + path);
+            var iconsForFolder = FolderColorsContainer.Load().GetFolderByColor(color);
+            var settings = RainbowFoldersSettings.Load();
+
+            var folder = settings.Folders.SingleOrDefault(x => x.Name == asset.name);
+            if (folder == null)
+            {
+                // add new
+                settings.Folders.Add(new RainbowFolder
                 {
                     Name = asset.name,
                     SmallIcon = iconsForFolder.SmallIcon,
@@ -57,7 +85,9 @@ namespace Borodar.RainbowFolders.Editor
             }
             else
             {
-                Debug.LogWarning("Can only colorize folders");
+                // modify existing
+                folder.SmallIcon = iconsForFolder.SmallIcon;
+                folder.LargeIcon = iconsForFolder.LargeIcon;
             }
         }
     }
