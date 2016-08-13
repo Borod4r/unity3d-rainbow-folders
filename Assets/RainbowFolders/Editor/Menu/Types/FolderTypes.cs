234bb868655e0d7ece454cee00186a1abcbf584c
mathiasdella <mathiasdella@hotmail.com>
2016-08-13 17:11:45 -0300

    Added MenuItem/Type
    Allows to quick change the folder to a type via the right-click menu
    just like it's done with colors and rags

---
+++ Assets/RainbowFolders/Editor/Menu/Types/FolderTypes.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Borodar.RainbowFolders.Editor
+{
+    public enum FolderTypes
+    {
+        Default = -1,
+
+        Prefabs = 0,
+        Scenes = 1,
+        Scripts = 2,
+        Extensions = 3,
+        Plugins = 4,
+        Textures = 5,
+        Materials = 6,
+        Audio = 7,
+        Brackets = 8,
+        Fonts = 9,
+        Editor = 10,
+        Resources = 11,
+        Shaders = 12,
+        Terrains = 13,
+        Meshes = 14,
+        Rainbow = 15
+
+    }
+}
---
+++ Assets/RainbowFolders/Editor/Menu/Types/FolderTypesStorage.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using Borodar.RainbowFolders.Editor.Settings;
+using System.IO;
+
+namespace Borodar.RainbowFolders.Editor
+{
+    public class FolderTypesStorage : ScriptableObject
+    {
+        public const string FOLDER_TYPE_STORAGE_ASSET_NAME = "RainbowTypeFoldersIconsStorage";
+
+        public List<RainbowTypeFolder> TypeFolderIcons;
+
+        private static FolderTypesStorage instance;
+
+        public static FolderTypesStorage Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var typeStorageAssetPath = GetTypeStorageAssetPath();
+                    if ((instance = EditorGUIUtility.Load(typeStorageAssetPath) as FolderTypesStorage) == null)
+                    {
+                        if (!Directory.Exists(Path.Combine(Application.dataPath, RainbowFoldersSettings.SETTINGS_PATH)))
+                        {
+                            AssetDatabase.CreateFolder("Assets", RainbowFoldersSettings.SETTINGS_PATH);
+                        }
+
+                        RainbowFoldersEditorUtility.CreateAsset<FolderColorsStorage>(FOLDER_TYPE_STORAGE_ASSET_NAME,
+                            Path.Combine("Assets", RainbowFoldersSettings.SETTINGS_PATH));
+                        instance = EditorGUIUtility.Load(typeStorageAssetPath) as FolderTypesStorage;
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private static string GetTypeStorageAssetPath()
+        {
+            string assetNameWithExtension = string.Join(".", new[]
+                {
+                    FOLDER_TYPE_STORAGE_ASSET_NAME,
+                    RainbowFoldersSettings.SETTINGS_ASSET_EXTENSION
+                });
+            string settingsPath = Path.Combine(RainbowFoldersSettings.SETTINGS_FOLDER, assetNameWithExtension);
+            return settingsPath;
+        }
+
+        public FolderIconPair GetIconsByType(FolderTypes type)
+        {
+            var colorFolder = TypeFolderIcons.Single(x => x.Type == type);
+            return new FolderIconPair { SmallIcon = colorFolder.SmallIcon, LargeIcon = colorFolder.LargeIcon };
+        }
+    }
+}
