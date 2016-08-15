775892532703936e012af328da9fb75aaef3ea78
Taras Leskiv <taras@getsocial.im>
2016-08-15 14:21:33 +0200

    folder type context menu

--- Assets/RainbowFolders/Editor/Menu/Types/FoldersTypesContainerEditor.cs
+++ Assets/RainbowFolders/Editor/Menu/Types/FoldersTypesContainerEditor.cs
@@ -1,4 +1,18 @@
-﻿using Borodar.ReorderableList;
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using Borodar.ReorderableList;
 using UnityEditor;
 using UnityEngine;
 
@@ -22,7 +36,7 @@ namespace Borodar.RainbowFolders.Editor
             EditorGUILayout.HelpBox("This is internal file for RainbowFolders. Do not edit.", MessageType.Warning);
 
             serializedObject.Update();
-            ReorderableListGUI.Title("Internal storage of colorful folder icons");
+            ReorderableListGUI.Title("Internal storage of folder types icons");
             ReorderableListGUI.ListField(_foldersProperty);
             serializedObject.ApplyModifiedProperties();
         }
